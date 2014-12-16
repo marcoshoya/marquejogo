617897e911a788a9a87e4a2111a784afdf36617f
Marcos Joia <marcoshoya@gmail.com>
2014-12-16 16:32:17 -0200

    fixing security routes

--- src/Marcoshoya/MarquejogoBundle/Controller/Adm/LoginController.php
+++ src/Marcoshoya/MarquejogoBundle/Controller/Adm/LoginController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Marcoshoya\MarquejogoBundle\Controller;
+namespace Marcoshoya\MarquejogoBundle\Controller\Adm;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -8,15 +8,19 @@ use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
-class AdmLoginController extends Controller
+class LoginController extends Controller
 {
+    /**
+     * @Route("/", name="_adm_dash")
+     * @Template()
+     */
     public function indexAction()
     {
         
     }
     
     /**
-     * @Route("/login", name="_demo_login")
+     * @Route("/login", name="_marquejogo_adm_login")
      * @Template()
      */
     public function loginAction(Request $request)
