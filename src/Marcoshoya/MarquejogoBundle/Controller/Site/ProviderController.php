b02fe152fc6a0e1ad7e4e8762a8929b1c727667d
Marcos Joia <marcoshoya@gmail.com>
2015-02-06 17:55:51 -0200

    add sidebar form

--- src/Marcoshoya/MarquejogoBundle/Controller/Site/ProviderController.php
+++ src/Marcoshoya/MarquejogoBundle/Controller/Site/ProviderController.php
@@ -14,6 +14,7 @@ use Marcoshoya\MarquejogoBundle\Entity\Schedule;
 use Marcoshoya\MarquejogoBundle\Form\ScheduleType;
 use Marcoshoya\MarquejogoBundle\Form\BookingItemType;
 use Marcoshoya\MarquejogoBundle\Form\AvailType;
+use Marcoshoya\MarquejogoBundle\Form\SearchType;
 
 /**
  * ProviderController implements all provider functions on site
@@ -45,6 +46,9 @@ class ProviderController extends Controller
         // form avail
         $formAvail = $this->createAvailForm($provider, $serviceSearch->getSearchData());
 
+        // form sidebar
+        $formSidebar = $this->createSidebarForm($provider);
+
         // get products available to sell
         $products = $this->getProducts($provider, $searchDTO);
 
@@ -54,6 +58,7 @@ class ProviderController extends Controller
                     'provider' => $provider,
                     'pictures' => $picture,
                     'formAvail' => $formAvail->createView(),
+                    'formSidebar' => $formSidebar->createView(),
             ));
         }
 
@@ -80,6 +85,7 @@ class ProviderController extends Controller
             'products' => $products,
             'form' => $form->createView(),
             'formAvail' => $formAvail->createView(),
+            'formSidebar' => $form->createView(),
         );
     }
 
@@ -113,6 +119,9 @@ class ProviderController extends Controller
             return $this->redirect($this->generateUrl('provider_show', array('id' => $provider->getId())));
         }
 
+        // form sidebar
+        $formSidebar = $this->createSidebarForm($provider);
+
         // get products available to sell
         $products = $this->getProducts($provider, $searchDTO);
 
@@ -122,6 +131,7 @@ class ProviderController extends Controller
                     'provider' => $provider,
                     'pictures' => $picture,
                     'formAvail' => $formAvail->createView(),
+                    'formSidebar' => $formSidebar->createView(),
             ));
         }
 
@@ -133,9 +143,72 @@ class ProviderController extends Controller
             'products' => $products,
             'form' => $form->createView(),
             'formAvail' => $formAvail->createView(),
+            'formSidebar' => $formSidebar->createView(),
         );
     }
 
+    /**
+     * @Route("/quadra{id}/doSearch", name="provider_dosearch")
+     * @Method("POST")
+     * @ParamConverter("provider", class="MarcoshoyaMarquejogoBundle:Provider")
+     * @Template("MarcoshoyaMarquejogoBundle:Site/Provider:show.html.twig")
+     */
+    public function dosearchAction(Request $request, Provider $provider)
+    {
+        // load services
+        $servicePerson = $this->get('marcoshoya_marquejogo.service.person');
+        $serviceSearch = $this->get('marcoshoya_marquejogo.service.search');
+
+        // search data
+        $searchDTO = $serviceSearch->getSearchSession();
+
+        // get pictures
+        $picture = $servicePerson->getAllPicture($provider);
+
+        // form avail
+        $formAvail = $this->createAvailForm($provider, $serviceSearch->getSearchData());
+
+        // sidebar
+        $formSidebar = $this->createSidebarForm($provider);
+
+        $formSidebar->handleRequest($request);
+        if ($formSidebar->isValid()) {
+
+            // gets the service
+            $service = $this->get('marcoshoya_marquejogo.service.search');
+            // form data
+            $data = $form->getData();
+            $service->setSearchSession($data);
+
+            $slug = BundleHelper::sluggable($data['city']);
+
+            return $this->redirect($this->generateUrl('search_result', array('city' => $slug)));
+        }
+
+        // get products available to sell
+        $products = $this->getProducts($provider, $searchDTO);
+
+        // if there is no product, it renders another template
+        if (!count($products)) {
+            return $this->render('MarcoshoyaMarquejogoBundle:Site/Provider:noresult.html.twig', array(
+                    'provider' => $provider,
+                    'pictures' => $picture,
+                    'formAvail' => $formAvail->createView(),
+                    'formSidebar' => $formSidebar->createView(),
+            ));
+        }
+
+        $form = $this->createScheduleForm($provider, $products);
+
+        return array(
+            'provider' => $provider,
+            'pictures' => $picture,
+            'products' => $products,
+            'form' => $form->createView(),
+            'formAvail' => $formAvail->createView(),
+            'formSidebar' => $formSidebar->createView(),
+        );
+    }
 
     /**
      * Get products to sell
@@ -204,6 +277,24 @@ class ProviderController extends Controller
         $searchData['provider'] = $provider->getId();
         $form = $this->createForm(new AvailType(), $searchData, array(
             'action' => $this->generateUrl('provider_doavail', array('id' => $provider->getId())),
+            'method' => 'POST',
+        ));
+
+        return $form;
+    }
+
+    /**
+     * Creates the search form
+     *
+     * @return SearchType
+     */
+    private function createSidebarForm(Provider $provider)
+    {
+        $service = $this->get('marcoshoya_marquejogo.service.search');
+        $data = $service->getSearchData();
+
+        $form = $this->createForm(new SearchType(), $data, array(
+            'action' => $this->generateUrl('provider_dosearch', array('id' => $provider->getId())),
             'method' => 'POST',
         ));
 
--- src/Marcoshoya/MarquejogoBundle/Controller/Site/SearchController.php
+++ src/Marcoshoya/MarquejogoBundle/Controller/Site/SearchController.php
@@ -2,11 +2,14 @@
 
 namespace Marcoshoya\MarquejogoBundle\Controller\Site;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Marcoshoya\MarquejogoBundle\Component\Search\SearchDTO;
+use Marcoshoya\MarquejogoBundle\Form\SearchType;
+use Marcoshoya\MarquejogoBundle\Helper\BundleHelper;
 
 /**
  * Search controller.
@@ -15,28 +18,30 @@ use Marcoshoya\MarquejogoBundle\Component\Search\SearchDTO;
  */
 class SearchController extends Controller
 {
+
     /**
      * Header action
      *
      * @return string
      */
-    public function sidebarAction()
+    public function sidebarAction($formSidebar)
     {
-        return $this->render('MarcoshoyaMarquejogoBundle:Site/Search:sidebar.html.twig');
+        return $this->render('MarcoshoyaMarquejogoBundle:Site/Search:sidebar.html.twig', array(
+                'formSidebar' => $formSidebar,
+        ));
     }
 
     /**
      * List all results from search
      *
      * @Route("/{city}", name="search_result")
-     * @Method("GET")
      * @Template()
      */
-    public function resultAction($city)
+    public function resultAction(Request $request, $city)
     {
         $searchService = $this->get('marcoshoya_marquejogo.service.search');
         $searchDTO = new SearchDTO();
-        
+
         $session = $this->get('session');
         if ($session->has(SearchDTO::session)) {
             $object = $session->get(SearchDTO::session);
@@ -46,13 +51,49 @@ class SearchController extends Controller
             $autocomplete = $this->get('marcoshoya_marquejogo.service.autocomplete')->getCity($city);
             $searchDTO->setAutocomplete($autocomplete);
         }
-        
+
         $searchResult = $searchService->doSearch($searchDTO);
-        
-        
+
+        $form = $this->createSearchForm($city);
+
+        if ($request->isMethod('POST')) {
+            $form->handleRequest($request);
+            if ($form->isValid()) {
+                // gets the service
+                $service = $this->get('marcoshoya_marquejogo.service.search');
+                // form data
+                $data = $form->getData();
+                $service->setSearchSession($data);
+
+                $slug = BundleHelper::sluggable($data['city']);
+
+                return $this->redirect($this->generateUrl('search_result', array('city' => $slug)));
+            }
+        }
+
         return array(
             'city' => $city,
-            'searchResult' => $searchResult
+            'searchResult' => $searchResult,
+            'formSidebar' => $form->createView(),
         );
     }
+
+    /**
+     * Creates the search form
+     * 
+     * @return SearchType
+     */
+    private function createSearchForm($city)
+    {
+        $service = $this->get('marcoshoya_marquejogo.service.search');
+        $data = $service->getSearchData();
+
+        $form = $this->createForm(new SearchType(), $data, array(
+            'action' => $this->generateUrl('search_result', array('city' => $city)),
+            'method' => 'POST',
+        ));
+
+        return $form;
+    }
+
 }
